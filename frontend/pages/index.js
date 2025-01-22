0386d44500e762bcfb7e4d684215e7f9bcda2bef
stazman <cadistasio@gmail.com>
2025-01-22 17:55:17 -0600

    Reorganizes frontend file system; frontend runs

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Speak2Me Fitness</h1>
+      <p>Your fitness companion powered by AI.</p>
+    </div>
+  );
+};
+
+export default Home;
