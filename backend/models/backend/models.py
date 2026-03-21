562ef4b23bd63b401012f66661715015b907b37c
stazman <cadistasio@gmail.com>
2026-03-20 21:29:47 -0500

    Adds routes files

---
+++ backend/models/backend/models.py
@@ -0,0 +1,19 @@
+from beanie import Document
+from pydantic import Field
+from typing import Optional
+from datetime import datetime
+
+class FoodLog(Document):
+    user_id: str
+    raw_input: str
+    food_name: str
+    calories: Optional[float] = None
+    protein: Optional[float] = None
+    carbs: Optional[float] = None
+    fat: Optional[float] = None
+    quantity: Optional[str]
+    logged_at: datetime = Field(default_factory=datetime.utcnow)
+
+class Settings:
+    name = "food_logs"
+
