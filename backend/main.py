a883da5685f242441375806e42d4a9e0d6231a26
stazman <cadistasio@gmail.com>
2026-04-13 12:22:17 -0500

    Updates allow_origins for Vercel

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://s2m-fitness-monorepo.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
